cccd6dfb3aea62ab023f466a72567dc17f1abd19
Zigulaj <hubi0864@gmail.com>
2024-05-21 15:32:02 +0200

    Dodałem trajektorie orbit, księżyc i pierścień saturna.

--- Projekt/Projekt.cpp
+++ Projekt/Projekt.cpp
@@ -14,6 +14,10 @@ void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 unsigned int loadShader(const char* vertexPath, const char* fragmentPath);
+std::vector<float> generateFlatRingVertices(float radius, float ringWidth, int segments);
+std::vector<unsigned int> generateFlatRingIndices(int segments);
+void drawOrbit(float radius, int segments, glm::mat4 view, glm::mat4 projection, unsigned int shaderProgram);
+
 
 // Camera settings
 glm::vec3 cameraPos = glm::vec3(18.0f, 50.0f, 20.0f);
@@ -153,20 +157,20 @@ int main() {
     // Define positions for the planets
     glm::vec3 planetPositions[] = {
         glm::vec3(0.0f, 0.0f, 0.0f),   // Slonce
-        glm::vec3(5.0f, 0.0f, 0.0f),   // Merkury
-        glm::vec3(8.0f, 0.0f, 0.0f),    // Wenus
-        glm::vec3(10.0f, 0.0f, 0.0f),    // Ziemia
-        glm::vec3(13.0f, 0.0f, 0.0f),    // Mars
-        glm::vec3(20.0f, 0.0f, 0.0f),    // Jowisz
-        glm::vec3(28.0f, 0.0f, 0.0f),    // Saturn
-        glm::vec3(36.0f, 0.0f, 0.0f),    // Uran
-        glm::vec3(44.0f, 0.0f, 0.0f),    // Neptun
+        glm::vec3(8.0f, 0.0f, 0.0f),   // Merkury
+        glm::vec3(11.0f, 0.0f, 0.0f),    // Wenus
+        glm::vec3(15.0f, 0.0f, 0.0f),    // Ziemia
+        glm::vec3(18.0f, 0.0f, 0.0f),    // Mars
+        glm::vec3(25.0f, 0.0f, 0.0f),    // Jowisz
+        glm::vec3(35.0f, 0.0f, 0.0f),    // Saturn
+        glm::vec3(45.0f, 0.0f, 0.0f),    // Uran
+        glm::vec3(53.0f, 0.0f, 0.0f),    // Neptun
         // Add more planet positions as needed
     };
-    float size_factor = 0.5f;
+    float size_factor = 0.6f;
     glm::vec3 planetScales[] = {
-        glm::vec3(5.0f, 5.0f, 5.0f),   // Slonce
-        glm::vec3(size_factor*0.38, size_factor * 0.38, size_factor * 0.38),         // Merkury
+        glm::vec3(10.0f, 10.0f, 10.0f),   // Slonce
+        glm::vec3(size_factor*0.45, size_factor * 0.45, size_factor * 0.45),         // Merkury
         glm::vec3(size_factor * 0.949, size_factor * 0.949, size_factor * 0.949),    // Wenus
         glm::vec3(size_factor, size_factor, size_factor),                            // Ziemia
         glm::vec3(size_factor*0.53, size_factor * 0.53, size_factor * 0.53),         // Mars
@@ -179,14 +183,14 @@ int main() {
     float speed_factor = 40.0f;
     float orbitSpeeds[] = {
         0.0f,
-        speed_factor * 4,   // merkury
+        speed_factor*4,   // merkury
         speed_factor * 1.62,  // wenus
         speed_factor,  // ziemia
         speed_factor * 0.53,  // mars
         speed_factor * 0.084f,  // jowisz
         speed_factor * 0.033 ,  // saturn
         speed_factor * 0.012,  // uran
-        speed_factor * 0,006,  // neptun
+        speed_factor * 0.006,  // neptun
     };
 
     float orbitAngles[] = {
@@ -215,9 +219,35 @@ int main() {
     };
 
     glm::vec3 sunColor = glm::vec3(1.0f, 1.0f, 0.0f); // Na przykład żółty
-    float glowRadius = 2.0f; // Na przykład promień 2 jednostek
+    float glowRadius = 10.0f; // Na przykład promień 2 jednostek
 
-    // Render loop
+    glm::vec3 moonPosition = glm::vec3(10.0f, 0.0f, 0.0f); // Pozycja Ziemi
+    float moonOrbitRadius = 1.0f; // Promień orbity Księżyca
+    float moonOrbitSpeed = speed_factor * 13.36f; // Szybkość orbity Księżyca
+    float moonOrbitAngle = 0.0f;
+
+    // Inicjalizacja płaskiego pierścienia Saturna
+    std::vector<float> flatRingVertices = generateFlatRingVertices(7.5f * size_factor, 1.7f, 36);
+    std::vector<unsigned int> flatRingIndices = generateFlatRingIndices(36);
+
+    unsigned int flatRingVBO, flatRingVAO, flatRingEBO;
+    glGenVertexArrays(1, &flatRingVAO);
+    glGenBuffers(1, &flatRingVBO);
+    glGenBuffers(1, &flatRingEBO);
+
+    glBindVertexArray(flatRingVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, flatRingVBO);
+    glBufferData(GL_ARRAY_BUFFER, flatRingVertices.size() * sizeof(float), flatRingVertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, flatRingEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, flatRingIndices.size() * sizeof(unsigned int), flatRingIndices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
    // Render loop
     while (!glfwWindowShouldClose(window)) {
         // Per-frame time logic
@@ -268,11 +298,22 @@ int main() {
             }
         }
 
+        // Update Moon's orbit angle
+        moonOrbitAngle += moonOrbitSpeed * deltaTime;
+        if (moonOrbitAngle > 360.0f) {
+            moonOrbitAngle -= 360.0f;
+        }
+
+        // Render the orbits
+        for (unsigned int i = 1; i < sizeof(planetPositions) / sizeof(glm::vec3); ++i) {
+            drawOrbit(glm::length(planetPositions[i]), 100, view, projection, shaderProgram);
+        }
+
         // Render the planets
         for (unsigned int i = 0; i < sizeof(planetPositions) / sizeof(glm::vec3); ++i) {
             glm::mat4 model = glm::mat4(1.0f);
 
-            // Apply translation and rotation for orbiting
+            // Orbita
             if (i > 0) {
                 float orbitRadius = glm::length(planetPositions[i]);
                 float x = cos(glm::radians(orbitAngles[i])) * orbitRadius;
@@ -282,37 +323,62 @@ int main() {
             else {
                 model = glm::translate(model, planetPositions[i]);
             }
-
-            model = glm::scale(model, planetScales[i]); // Apply scaling
+            model = glm::scale(model, planetScales[i]);
             glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
-            // Ustaw kolor planety
             glUniform3fv(glGetUniformLocation(shaderProgram, "planetColor"), 1, glm::value_ptr(planetColors[i]));
-
-            // Set whether this is the Sun
             if (i == 0) {
                 glUniform1i(glGetUniformLocation(shaderProgram, "isSun"), 1);
             }
             else {
                 glUniform1i(glGetUniformLocation(shaderProgram, "isSun"), 0);
             }
-
-            // Render a sphere for each planet
             glBindVertexArray(VAO);
             glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
+            // Renderowanie pierścienia Saturna
+            if (i == 6) {
+                glm::mat4 ringModel = glm::mat4(1.0f);
+                float saturnOrbitRadius = glm::length(planetPositions[6]);
+                float saturnX = cos(glm::radians(orbitAngles[6])) * saturnOrbitRadius;
+                float saturnZ = sin(glm::radians(orbitAngles[6])) * saturnOrbitRadius;
+                ringModel = glm::translate(ringModel, glm::vec3(saturnX, 0.0f, saturnZ));
+                glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(ringModel));
+                glBindVertexArray(flatRingVAO);
+                glDrawElements(GL_TRIANGLES, flatRingIndices.size(), GL_UNSIGNED_INT, 0);
+            }
+
+            // Renderowanie Księżyca
+            if (i == 3) {
+                glm::mat4 moonModel = glm::mat4(1.0f);
+                float earthOrbitRadius = glm::length(planetPositions[3]);
+                float earthX = cos(glm::radians(orbitAngles[3])) * earthOrbitRadius;
+                float earthZ = sin(glm::radians(orbitAngles[3])) * earthOrbitRadius;
+                float moonX = cos(glm::radians(moonOrbitAngle)) * moonOrbitRadius;
+                float moonZ = sin(glm::radians(moonOrbitAngle)) * moonOrbitRadius;
+                moonModel = glm::translate(moonModel, glm::vec3(earthX + moonX, 0.0f, earthZ + moonZ));
+                moonModel = glm::scale(moonModel, glm::vec3(size_factor * 0.273f, size_factor * 0.273f, size_factor * 0.273f));
+                glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(moonModel));
+                glUniform3fv(glGetUniformLocation(shaderProgram, "planetColor"), 1, glm::value_ptr(glm::vec3(0.8f, 0.8f, 0.8f)));
+                glUniform1i(glGetUniformLocation(shaderProgram, "isSun"), 0);
+                glBindVertexArray(VAO);
+                glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            }
         }
 
-        // Swap buffers and poll events
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // Clean up
+    glDeleteVertexArrays(1, &flatRingVAO);
+    glDeleteBuffers(1, &flatRingVBO);
+    glDeleteBuffers(1, &flatRingEBO);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-
     glfwTerminate();
+
     return 0;
 }
 
@@ -321,7 +387,7 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    float cameraSpeed = 5.5f * deltaTime; // Adjust accordingly
+    float cameraSpeed = 10.0f * deltaTime; // Adjust accordingly
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         cameraPos += cameraSpeed * cameraFront;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -379,6 +445,95 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
     if (fov >= 45.0f)
         fov = 45.0f;
 }
+
+std::vector<float> generateFlatRingVertices(float radius, float ringWidth, int segments) {
+    std::vector<float> vertices;
+    float innerRadius = radius - ringWidth / 2.0f;
+    float outerRadius = radius + ringWidth / 2.0f;
+
+    for (int i = 0; i <= segments; ++i) {
+        float theta = i * 2.0f * glm::pi<float>() / segments;
+        float cosTheta = cos(theta);
+        float sinTheta = sin(theta);
+
+        // Outer ring vertex
+        vertices.push_back(outerRadius * cosTheta);
+        vertices.push_back(0.0f); // flat ring, so y is 0
+        vertices.push_back(outerRadius * sinTheta);
+
+        // Inner ring vertex
+        vertices.push_back(innerRadius * cosTheta);
+        vertices.push_back(0.0f); // flat ring, so y is 0
+        vertices.push_back(innerRadius * sinTheta);
+    }
+    return vertices;
+}
+
+std::vector<unsigned int> generateFlatRingIndices(int segments) {
+    std::vector<unsigned int> indices;
+    for (int i = 0; i < segments; ++i) {
+        int first = i * 2;
+        int second = first + 1;
+        int nextFirst = (first + 2) % (2 * (segments + 1));
+        int nextSecond = (second + 2) % (2 * (segments + 1));
+
+        indices.push_back(first);
+        indices.push_back(nextFirst);
+        indices.push_back(second);
+
+        indices.push_back(second);
+        indices.push_back(nextFirst);
+        indices.push_back(nextSecond);
+    }
+    return indices;
+}
+
+void drawOrbit(float radius, int segments, glm::mat4 view, glm::mat4 projection, unsigned int shaderProgram) {
+    std::vector<float> vertices;
+    for (int i = 0; i <= segments; ++i) {
+        float theta = i * 2.0f * glm::pi<float>() / segments;
+        float x = radius * cos(theta);
+        float z = radius * sin(theta);
+        vertices.push_back(x);
+        vertices.push_back(0.0f);
+        vertices.push_back(z);
+    }
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Use the shader program
+    glUseProgram(shaderProgram);
+
+    // Set view and projection matrices
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    glUniform3fv(glGetUniformLocation(shaderProgram, "planetColor"), 1, glm::value_ptr(glm::vec3(1.5f, 1.5f, 1.5f)));
+
+    // Set the model matrix to identity
+    glm::mat4 model = glm::mat4(1.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+    // Draw the orbit
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINE_LOOP, 0, segments);
+
+    // Clean up
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+}
+
+
+
 
 // Utility function for loading a shader
 unsigned int loadShader(const char* vertexPath, const char* fragmentPath) {
